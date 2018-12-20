e5d49f8db6e32fc17f0506f64b69ec4ad45ce5ff
MrEmojiSourir <41867615+MrEmojiSourir@users.noreply.github.com>
2018-12-20 18:47:32 +0100

    Update testmacro.py

--- testmacro.py
+++ testmacro.py
@@ -4,28 +4,53 @@ import time
 import win32api
 import win32con
 import sys
+from color import *
 
 os.system("CLS")
-print("Microsoft Windows [version 10.0.15063]\n \
-(c) 2017 Microsoft Corporation. Tous droits réservés.")
+
+version = "Imagination du projet"
+logiciel_name = "Beurre"
+
+fondateur = [
+    "MrEmojiSourir",
+    "link1183"
+]
+
+developeur = [
+    "Bisskote",
+    "MrEmojiSourir",
+    "link1183"
+]
+administrateur = [
+    "MrEmojiSourir",
+    "link1183",
+    "{ Cecemel_PvP }"
+]
 
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-time.sleep(2)
-while True:
-    # keyboard.write("z")
-    # keyboard.press_and_release('<')
+# def click(x,y):
+#     win32api.SetCursorPos((x,y))
+#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+# time.sleep(2)
+
+# while True:
+#     # keyboard.write("z")
+#     # keyboard.press_and_release('<')
     
 
-    clickon = True
-    nb = 0
-    while clickon:
-        click(960,540)
-        nb =+ 1 
-        if nb == 50:
-            clickon = False
+#     clickon = True
+#     nb = 0
+#     while clickon:
+#         click(960,540)
+#         nb =+ 1 
+#         if nb == 50:
+#             clickon = False
 
-    time.sleep(1)
+#     time.sleep(1)
+
+print(f"Merci d'utiliser {logiciel_name} V{version}")
+print(f" Créer par : Fondateur : {fondateur},\n Developeur : {developeur},\n Administrateur : {administrateur}")
+
+sys.stdout.write(RED)
+print("test !")
